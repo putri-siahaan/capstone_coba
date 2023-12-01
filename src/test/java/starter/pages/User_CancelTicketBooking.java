916e0daa6d354c5fb0266e187e7e70516cc9b2b0
putrisiahaan <89520089+ifs20043-itdel@users.noreply.github.com>
2023-12-01 15:47:52 +0700

    alhamdulilah pasti bisa cicd

--- src/test/java/starter/pages/User_CancelTicketBooking.java
+++ src/test/java/starter/pages/User_CancelTicketBooking.java
@@ -13,7 +13,7 @@ public class User_CancelTicketBooking {
 
     @Step("User input endpoint with valid invoice number to cancel booking ticket")
     public String userInputEndpointWIthValidInvoiceNumberToCancelTicket(){
-        return url + "user/ticket/1701419819-983";
+        return url + "user/ticket/1701420336-507";
     }
 
     @Step("User request with HTTP method DELETE to cancel booking ticket and click Send Button")
